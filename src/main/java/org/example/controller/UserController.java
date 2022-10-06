0fcc07c9effa73130d15b6d7c94669c164cfece3
Seth Post <postsa@miamioh.edu>
2022-10-06 14:09:41 -0400

    Added other functions and endpoints to UserController. Started making create.sh file

---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,45 @@
+package org.example.controller;
+
+import org.example.dao.UserDao;
+import org.example.model.User;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+//I would include a @PreAuthorize annotation here if I were implementing that in the project
+public class UserController {
+
+    @Autowired
+    private UserDao userDao;
+
+    public UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @GetMapping("/users-ascending/?q={searchQuery}")
+    public List<User> getUsersByNameAscending(@PathVariable String searchQuery) {
+        return userDao.getUsersByNameAscending(searchQuery);
+    }
+
+    @GetMapping("/users-descending/?q={searchQuery}")
+    public List<User> getUsersByNameDescending(@PathVariable String searchQuery) {
+        return userDao.getUsersByNameDescending(searchQuery);
+    }
+
+    @GetMapping("/users-ascending-paginated/?q={searchQuery}&n={usersPerPage}")
+    public List<List<User>> getUsersByNameAscendingPaginated(@PathVariable String searchQuery, @PathVariable int usersPerPage) {
+        return userDao.getUsersByNameAscendingPaginated(searchQuery, usersPerPage);
+    }
+
+    @GetMapping("/users-descending-paginated/?q={searchQuery}&n={usersPerPage}")
+    public List<List<User>> getUsersByNameDescendingPaginated(@PathVariable String searchQuery, @PathVariable int usersPerPage) {
+        return userDao.getUsersByNameDescendingPaginated(searchQuery, usersPerPage);
+    }
+
+}
