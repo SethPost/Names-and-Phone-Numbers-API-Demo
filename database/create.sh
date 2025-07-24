f03ae392734c95fdc021a19bb9eff6e9917e9f43
SethPost <sethapost@gmail.com>
2025-07-24 12:46:30 -0400

    Some refactoring and updates to application.properties after revisiting project

--- database/create.sh
+++ database/create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PGPASSWORD='postgres1'
 BASEDIR=$(dirname $0)
-DATABASE=interviewDB
-psql -U postgres -f "$BASEDIR/dropdb.sql" &&
+DATABASE=interviewdb
+dropdb -U postgres --if-exists $DATABASE &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
